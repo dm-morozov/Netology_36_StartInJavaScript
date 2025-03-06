20e505cb328178e5748f4eed28f4b23736be69c8
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2025-03-06 11:28:09 +0500

    Выполнено домашнее задание к занятию 2.1 DOM. Ротатор рекламы. | Completed homework for lesson 2.1 DOM. Ad Rotator.

---
+++ 6_DOM/ads/task.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const rotators = document.querySelectorAll('.rotator');
+
+    let speed = 1000,
+        color = 'red';
+
+    function checkVisivility() {
+
+        rotators.forEach(rotator => {
+
+            const rotatorCase = document.querySelector('.rotator__case.rotator__case_active');
+            rotatorCase.classList.remove('rotator__case_active');
+            
+            let nextElement;
+            if (rotatorCase.nextElementSibling) {
+                nextElement = rotatorCase.nextElementSibling;
+            } else {
+                nextElement = rotator.firstElementChild;
+            }
+
+            nextElement.classList.add('rotator__case_active');
+            nextElement.style.color = nextElement.dataset.color;
+            speed = parseInt(nextElement.dataset.speed) || 1000;
+            console.log('Элемент на странице: ', nextElement);
+            setTimeout(checkVisivility, speed);
+        });
+
+    }
+
+    checkVisivility()
+});
